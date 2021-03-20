c3455a7628aba1602baa2df2b41b86a9a08bf09d
SvetlanaFoglia <luce62006@gmail.com>
2021-03-20 16:41:46 +0100

    step 2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/Luce62006/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Luce62006/frontend-project-lvl2/actions)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,9 @@ const program = new commander.Command();
 
 program
   .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
-console.log('hello');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
+// eslint-disable-next-line no-console
 
 program.parse(process.argv);
+
