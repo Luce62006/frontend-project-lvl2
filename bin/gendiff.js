680da3a5cab04c3a42966c652c1db69c55fc09f3
SvetlanaFoglia <luce62006@gmail.com>
2021-03-22 22:06:25 +0100

    3step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
+import genDiff from '../index.js';
 
 const program = new commander.Command();
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
-// eslint-disable-next-line no-console
-
-program.parse(process.argv);
+  .arguments('<pathToFile1> <pathToFile2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action(
+    (pathToFile1, pathToFile2) => {
+      console.log(genDiff(pathToFile1, pathToFile2, program.format));
+    },
+  )
+  .parse(process.argv);
